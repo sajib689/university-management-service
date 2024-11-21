0cf23144521d0360f861aa5369ece041cb7a02cb
sajib689 <jibonraja11@gmail.com>
2024-11-21 21:55:24 +0600

    get the env data from the index.ts file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,8 @@
 import dotenv from 'dotenv'
 import path from 'path'
 dotenv.config({path: path.join(process.cwd(), '.env')})
+
+export default {
+    port: process.env.PORT,
+    nodeEnv: process.env.NODE_ENV
+}
