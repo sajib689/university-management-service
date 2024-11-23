e18588fad89d8400384b52557671b49d2b80f7d4
sajib689 <jibonraja11@gmail.com>
2024-11-23 12:30:13 +0600

    add the prettier success

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
-import mongoose from "mongoose";
-import config from "./config/index";
-import dotenv from 'dotenv'
-dotenv.config()
-import app from './app'
-const port:number = Number(config.port)
+import mongoose from 'mongoose';
+import config from './config/index';
+import dotenv from 'dotenv';
+dotenv.config();
+import app from './app';
+const port: number = Number(config.port);
 async function bootstrap() {
-    try {
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2m0rny5.mongodb.net/university-managements-service`)
-        app.listen(port, () => {
-            console.log(`Listening on ${port}`)
-        })
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2m0rny5.mongodb.net/university-managements-service`
+    );
+    app.listen(port, () => {
+      console.log(`Listening on ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
-bootstrap()
+bootstrap();
