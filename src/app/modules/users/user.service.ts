015d27b0d84cd6851c738497595b5a33930a6bb7
sajib689 <jibonraja11@gmail.com>
2024-11-27 23:30:01 +0600

    add the user id

---
+++ src/app/modules/users/user.service.ts
@@ -0,0 +1,22 @@
+import config from '../../../config';
+import { generateUserId } from './user.utils';
+import { IUser } from './users.interface';
+import { User } from './users.model';
+
+const createUser = async (user: IUser): Promise<IUser | null> => {
+  const id = await generateUserId();
+  user.id = id;
+  if (!user.password) {
+    user.password = config.defaultUserPasswords as string;
+  }
+  const createdUser = await User.create(user);
+
+  if (!createdUser) {
+    throw new Error(`Failed to create user`);
+  }
+  return createdUser;
+};
+
+export default {
+  createUser
+};
