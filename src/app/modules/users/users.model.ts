d0c8e89811814252e2f4dcf654f46154b3b0a07c
sajib689 <jibonraja11@gmail.com>
2024-11-27 21:43:52 +0600

    add the model for the user data

--- src/app/modules/users/users.model.ts
+++ src/app/modules/users/users.model.ts
@@ -1,10 +1,25 @@
-import { Schema } from 'mongoose';
+import { model, Model, Schema } from 'mongoose';
 import { IUser } from './users.interface';
 
-export const userSchema = new Schema<IUser>({
-  id: {
-    type: String,
-    required: true,
-    unique: true
+type userModel = Model<IUser, object>;
+export const userSchema = new Schema<IUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    role: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
   }
-});
+);
+export const User = model<IUser, userModel>('User', userSchema);
