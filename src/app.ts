af8585fc747608a5c43c605a05476bf0dd52432a
sajib689 <jibonraja11@gmail.com>
2024-11-29 10:13:05 +0600

    user route created successfully and test it succes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import usersRoute from './app/modules/users/users.route';
 const app: Application = express();
 // parser
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// router
+app.use('/api/v1/users/', usersRoute);
 // Testing server
 app.get('/', (req: Request, res: Response) => {
   res.send('Working directory');
