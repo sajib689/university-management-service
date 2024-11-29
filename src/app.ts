531a3c1e24b7e9d36b7f5fc8f2a07ad577481d87
sajib689 <jibonraja11@gmail.com>
2024-11-29 10:51:06 +0600

    add the logger on the project

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // router
+console.log(app.get('env'));
 app.use('/api/v1/users/', usersRoute);
 // Testing server
 app.get('/', (req: Request, res: Response) => {
