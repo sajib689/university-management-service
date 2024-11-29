e2755012cfe9f17502b45ed7ce543e6f595f67ed
sajib689 <jibonraja11@gmail.com>
2024-11-29 12:17:15 +0600

    add the winston roate file

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,7 +1,7 @@
 import { createLogger, format, transports } from 'winston';
 import path from 'path';
 import { TransformableInfo } from 'logform';
-
+import DailyRotateFile from 'winston-daily-rotate-file';
 const { combine, timestamp, label, printf } = format;
 
 // Properly typed custom formatting
@@ -31,7 +31,18 @@ const logger = createLogger({
   transports: [
     new transports.Console(),
     new transports.File({
-      filename: path.join(process.cwd(), '/logs/winston/success.log')
+      filename: path.join(
+        process.cwd(),
+        '/logs/winston/success/phu-%DATE%.log'
+      ),
+      level: 'info'
+    }),
+    new DailyRotateFile({
+      filename: 'application-%DATE%.log',
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d'
     })
   ]
 });
@@ -44,7 +55,15 @@ const errorLogger = createLogger({
   transports: [
     new transports.Console(),
     new transports.File({
-      filename: path.join(process.cwd(), '/logs/winston/error.log')
+      filename: path.join(process.cwd(), '/logs/winston/errors/phu-%DATE%.log'),
+      level: 'error'
+    }),
+    new DailyRotateFile({
+      filename: 'application-%DATE%.log',
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d'
     })
   ]
 });
