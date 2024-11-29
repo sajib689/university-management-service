8f1bfe89210221c4a4cc0ba10b92173135a24822
sajib689 <jibonraja11@gmail.com>
2024-11-29 11:06:01 +0600

    add new error logger and success error

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import config from './config/index';
 import dotenv from 'dotenv';
 dotenv.config();
 import app from './app';
+import { logger, errorLogger } from './shared/logger';
 const port: number = Number(config.port);
 async function bootstrap() {
   try {
@@ -10,10 +11,10 @@ async function bootstrap() {
       `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2m0rny5.mongodb.net/university-managements-service`
     );
     app.listen(port, () => {
-      console.log(`Listening on ${port}`);
+      logger.info(`Listening on ${port}`);
     });
   } catch (error) {
-    console.log(error);
+    errorLogger.error(error);
   }
 }
 bootstrap();
--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -4,18 +4,14 @@ const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with importance level of `error` or higher to `error.log`
-    //   (i.e., error, fatal, but not other levels)
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    //
-    // - Write all logs with importance level of `info` or higher to `combined.log`
-    //   (i.e., fatal, error, warn, and info, but not trace)
-    //
-    new winston.transports.File({ filename: 'combined.log' })
-  ]
+  transports: [new winston.transports.Console()]
 });
 
-export default logger;
+const errorLogger = winston.createLogger({
+  level: 'error',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [new winston.transports.Console()]
+});
+
+export { logger, errorLogger };
