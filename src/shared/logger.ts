e336c06d1c9d3bb5793fdc834423f0316d16b049
sajib689 <jibonraja11@gmail.com>
2024-11-29 11:16:18 +0600

    add the log directory

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,17 +1,27 @@
 import winston from 'winston';
-
+import path from 'path';
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   defaultMeta: { service: 'user-service' },
-  transports: [new winston.transports.Console()]
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), '/logs/winston/success.log')
+    })
+  ]
 });
 
 const errorLogger = winston.createLogger({
   level: 'error',
   format: winston.format.json(),
   defaultMeta: { service: 'user-service' },
-  transports: [new winston.transports.Console()]
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), '/logs/winston/error.log')
+    })
+  ]
 });
 
 export { logger, errorLogger };
