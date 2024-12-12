97a2fecf58ffc08f282628f40dfddba8eb1bb8f9
sajib689 <jibonraja11@gmail.com>
2024-12-12 17:20:39 +0600

    add controller file and the service file complete

---
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -0,0 +1,13 @@
+import { IAcademicSemester } from './academicSemester.interface';
+import { AcademicSemester } from './academicSemester.model';
+
+const createAcademicSemester = async (
+  academicSemester: IAcademicSemester
+): Promise<IAcademicSemester | null> => {
+  const createdSemester = await AcademicSemester.create(academicSemester);
+  return createdSemester;
+};
+
+export default {
+  createAcademicSemester
+};
