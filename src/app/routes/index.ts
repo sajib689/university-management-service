0d3a19b10464d308646488bdd6d1638da71873dc
sajib689 <jibonraja11@gmail.com>
2024-12-16 18:49:43 +0600

    short the try catch function used by higher order function

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,19 @@
 import express from 'express';
 const router = express.Router();
-import usersRoute from '../modules/users/users.controller';
+import usersRoute from '../modules/users/users.route';
 import semesterRoute from '../modules/academicSemester/academicSemester.route';
 
-router.use('/users/', usersRoute);
-router.use('/semesters/', semesterRoute);
+const moduleRoutes = [
+  {
+    path: '/users',
+    route: usersRoute
+  },
+  {
+    path: '/semesters',
+    route: semesterRoute
+  }
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
 export default router;
