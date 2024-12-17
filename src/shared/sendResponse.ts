4300e4d317b438033ba7b8517a7cf76b970adb0a
sajib689 <jibonraja11@gmail.com>
2024-12-17 16:30:27 +0600

    add type for the sendResponse data

--- src/shared/sendResponse.ts
+++ src/shared/sendResponse.ts
@@ -1,20 +1,20 @@
 import { Response } from 'express';
 
-const sendResponse = <T>(
-  res: Response,
-  data: {
-    statusCode: number;
-    success: boolean;
-    message?: string | null;
-    data: T;
-  }
-) => {
-  res.status(data.statusCode).json({
+type IApiResponse<T> = {
+  statusCode: number;
+  success: boolean;
+  message?: string | null;
+  data: T;
+};
+
+const sendResponse = <T>(res: Response, data: IApiResponse<T>): void => {
+  const responseData = {
     statusCode: data.statusCode,
     success: data.success,
     message: data.message || null,
     data: data.data || null
-  });
+  };
+  res.status(data.statusCode).json(responseData);
 };
 
 export default sendResponse;
