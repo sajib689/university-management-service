60e6dd44cc54f2ce552be176b6dcdac09a44a1ca
sajib689 <jibonraja11@gmail.com>
2024-12-17 16:39:03 +0600

    send response function added on the users controller and the academic semester controller

--- src/app/modules/users/users.controller.ts
+++ src/app/modules/users/users.controller.ts
@@ -1,13 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
 import userService from './users.service';
 import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
 
 const createUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const { user } = req.body;
     const result = await userService.createUser(user);
     next();
-    res.status(200).json({
+
+    sendResponse(res, {
+      statusCode: 200,
       success: true,
       message: 'User created successfully',
       data: result
--- src/shared/sendResponse.ts
+++ src/shared/sendResponse.ts
@@ -4,11 +4,11 @@ type IApiResponse<T> = {
   statusCode: number;
   success: boolean;
   message?: string | null;
-  data: T;
+  data?: T | null;
 };
 
 const sendResponse = <T>(res: Response, data: IApiResponse<T>): void => {
-  const responseData = {
+  const responseData: IApiResponse<T> = {
     statusCode: data.statusCode,
     success: data.success,
     message: data.message || null,
