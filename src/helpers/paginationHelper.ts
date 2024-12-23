c192e3e82fb9d5df1388d54b26ed8cffb6495254
sajib689 <jibonraja11@gmail.com>
2024-12-23 11:30:13 +0600

    add pagination Helper

---
+++ src/helpers/paginationHelper.ts
@@ -0,0 +1,24 @@
+type IOptions = {
+  page?: number;
+  limit?: number;
+};
+
+type IOptionsResult = {
+  page: number;
+  limit: number;
+  skip: number;
+};
+
+const calculatePagination = (options: IOptions): IOptionsResult => {
+  const page = Number(options.page || 1);
+  const limit = Number(options.limit || 10);
+  const skip = (page - 1) * limit;
+
+  return {
+    page,
+    limit,
+    skip
+  };
+};
+
+export default calculatePagination;
